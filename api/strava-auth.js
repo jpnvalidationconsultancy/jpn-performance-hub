80999fc582c306eebd3747a9455108d80736817d
jpnvalidationconsultancy <jpnvalidationconsultancy@gmail.com>
2026-05-04 23:27:16 +0100

    Update strava-auth.js

--- api/strava-auth.js
+++ api/strava-auth.js
@@ -1,1 +1,14 @@
+export default async function handler(req, res) {
+  const clientId = process.env.STRAVA_CLIENT_ID;
+  const redirectUri = process.env.STRAVA_REDIRECT_URI;
 
+  const url = `https://www.strava.com/oauth/authorize
+    ?client_id=${clientId}
+    &response_type=code
+    &redirect_uri=${redirectUri}
+    &approval_prompt=auto
+    &scope=activity:read_all`;
+
+  res.writeHead(302, { Location: url.replace(/\s/g, "") });
+  res.end();
+}
